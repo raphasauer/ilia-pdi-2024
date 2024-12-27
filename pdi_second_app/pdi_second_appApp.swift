e4372aa13944cfdb39b31b8ae0d62ae84d21f0ff
Raphael Castro <Raphael.Castro@partner.bmw.com.br>
2024-12-27 15:11:29 -0300

    Initial Commit

---
+++ pdi_second_app/pdi_second_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  pdi_second_appApp.swift
+//  pdi_second_app
+//
+//  Created by Raphael de Castro on 27/12/24.
+//
+
+import SwiftUI
+
+@main
+struct pdi_second_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
